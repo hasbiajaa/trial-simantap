dd3ff71c4045c9909e49a0afb86870087fe15cff
Muhammad Hasbi <23400010.muhammad@tsu.ac.id>
2026-09-22 14:40:02 +0700

    Delete spi-backend directory

--- spi-backend/check_users.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('FCPATH', __DIR__ . '/public' . DIRECTORY_SEPARATOR);
-require FCPATH . '../app/Config/Paths.php';
-$paths = new Config\Paths();
-require rtrim($paths->systemDirectory, '\\/ ') . DIRECTORY_SEPARATOR . 'bootstrap.php';
-$users = model('UserModel');
-print_r($users->findAll());
