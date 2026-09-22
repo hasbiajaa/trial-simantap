b874007ac02305d268ce8f26b6c25a9020723f1d
Muhammad Hasbi <23400010.muhammad@tsu.ac.id>
2026-09-22 15:22:20 +0700

    update logo

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import { useState, type CSSProperties } from "react";
 import { ACCOUNTS, User } from "@/types/auth";
-import logoTSU from "@/assets/logo-tsu.png";
+import logoTSU from "src/imports/LOGO_TSU_png.png";
 
 const IconEye = ({ className = "" }) => (
   <svg className={className} viewBox="0 0 24 24" fill="currentColor">
